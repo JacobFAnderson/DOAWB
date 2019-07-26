d16446fc1a45ed831c803a7f53cfe38fd311c7e9
JacobFAnderson <53350770+JacobFAnderson@users.noreply.github.com>
2019-07-26 11:37:41 -0700

    Add files via upload

---
+++ DOAWB.py
@@ -0,0 +1,370 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                DEATH OF A WINGED BULLFROG
+###############################################################################
+###############################################################################
+#
+#  This is a work of fiction and any likeiness to real people is probably
+# intentional because you guys sure are weird. Weird enough to record somewhere
+#       providing a service
+#       ready for ass kissing
+#
+###############################################################################
+###############################################################################
+#
+#
+#
+import random
+import nouns
+import adjectives
+import verbs
+import sentences
+
+# sentences = list()
+#
+# sentences.append(" The Longest Night she ever knew")
+# sentences.append("By night or by water there's really little purpose")
+# sentences.append("The cusp of cliche that speaks smaller volumes than  you think")
+
+
+
+print "\n"
+
+import sys
+Title = "__" + nouns.get_noun(nouns.noun_list) + "_of_" + adjectives.get_adjective(adjectives.first)\
+        +" "+ nouns.get_noun(nouns.noun_list) + "__"
+
+Title.upper()
+
+print Title
+
+class Wednesday_24_1130(object):
+
+    def Gave_it_a_shot(self):
+
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "    all for the best\n"\
+        "pleasures for " + adjectives.get_adjective(adjectives.first) + " the best\n"\
+        "on a broadcast station that\n"\
+        "\t won't be written about in\n"\
+        "nostaglic forums, never bought up\n"\
+        "but that reboot would have been\n" \
+                 "  pretty awesome "
+        return string
+
+    def Girl_Whisperer(self):
+        print "\n\n" +sys._getframe().f_code.co_name + divider()
+        string = "Love on the third look \n"\
+        "better than the fourth \n"\
+        "which is a hometown divider \n"\
+        "If it doesn't happen by the third\n" \
+        "then fingers       crossed for the   fifth\n"\
+        "       if only you looked at the wrong " + nouns.get_noun(nouns.noun_list) +"\n"
+        return string
+
+    def Wildabeast(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "Kept to ourselves when it didn't matter\n"\
+        "and paid for her own words, hanging by shroom limelight\n"\
+        "put that shit        back\n"\
+        "Think this is a fucking joke?\n"\
+        "laughing all the way home with cups that\n" \
+                 "      soaked through\n" \
+                 "her eyes like this        if only\n"
+
+        return string
+
+
+    def WildabeastII(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "          Wept to the sound of vomtting in the car\n"\
+        "you have to pull your head back slightly, straighten out your bull stomach\n"\
+        "\n Throw the bag in the street because you missed the trashcan\n"\
+        "She'll never let me forget it\n"\
+        "\n but I did \n" \
+                 "          brine liquid that could eat up ships\n" \
+                 "In her eyes, she'll scold me for sushing her in \n"\
+        "in kung fu panda 3\n" \
+        "Never the way I want to throw up again"
+
+        return string
+
+    def Rose(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string ="charming " + adjectives.get_adjective(adjectives.first) +"    with     \n" \
+                "       middle name\n" \
+                "\n\na          lways call      me with an iphone\n" \
+                "pushed against a grill that never      holds \n"\
+                "the light just right\n"\
+                "\n           you better get that iphone\n"\
+                "   my words won't sound good otherwise"
+
+        return string
+
+    def Build_IT(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "determined to blow it out of proportion, I cheated, I didn't cheat enough    \n" \
+                 "  middle vowel between tongues and legs of obstructions \n" \
+                 "Never bold enough, the font, to catch a new infection\n" \
+                 "sick within hours of the excedrin and for three days after\n" \
+                 "\n I wrecked a toilet and felt bad enough to clean it before the orderlies did\n" \
+                 "\n That virus that people can smell on you, blue river between\n" \
+                 "killing and killed. Only letters seperate us from the dead"\
+
+        return string
+
+    def Best_Bundle2(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "      he  r tons of bullet mashed clothing \n" \
+                 "  ripp    ing the taste out of her mouth \n" \
+                 "Metal the last copper test\n" \
+                 "she   dies within h   ours\n" \
+                 "\n and left her bones for the res t of us\n"
+        return string
+
+class The_Hunk_and_I(object):
+
+    def VirtualGettinIt(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = " I gave her one, they gave him two\n\n" \
+        "two one, her gives they what they gave  "
+
+        return string
+
+    def Virtious_Game_Changer(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "More More More\n\n" \
+        "phine and ring with your arms up in the air\n"\
+        "always care, and always like the status\n"\
+        "you don't know what they will say about you next\n"
+        return string
+
+    def Too_Much_Time_With_Chronos(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = " as flustered I'm persisted\n" \
+        " carried This of sun, conquest. head. and no the must\n"\
+        "whole the world to see. Poor the pay! This is the bissmark\n"\
+
+        return string
+
+    def The_Romantics(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "Waste space touching old posters and flinging records off the cliff\n" \
+                 "The sound is crisp, a low low billow that brightens the corners   aces to places\n" \
+                 "The shards are black obsidion\n" \
+                "I burried the record out behind the tree I cut down for her\n"\
+                "First love is never enough love\n"\
+                "it lasts too               long\n"\
+                "burning and pacing the hand you've never drawn\n"\
+                "\n"\
+                "You know something honey? It won't end well\n"\
+                "for me, and it won't mean well for you\n"
+
+        return string
+
+    def Follow_with_care(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "Crafter with loose ships and sinking lips\n" \
+                 "Cant you see we are working?\n" \
+                 "\n hard on the tip of the dick\n" \
+                "Two modes, Horny and Hungry\n"\
+                "First kiss doesn't have anything\n"\
+                "on curb side blow jobs\n"\
+                "Miracle on the eight mile\n"\
+                "never letting her drive again\n"
+        return string
+
+
+    def Holding_Gold(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = " streaked from the hearth to the bathroom \n" \
+             " feet clicking on rapped tile and wet towels\n" \
+                 "He checks himself, again, and can't bend it back\n" \
+             " Wasted enough that it doesn't hurt\n" \
+             " but it's going too\n" \
+             " \n" \
+             " \n" \
+             " \n" \
+             " \n" \
+             " \n" \
+             " \n" \
+             " \n" \
+             " \n" \
+             " \n"
+        return string
+
+    def Marinol(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = " Alex smokes some fucking weed \n" \
+             "and pills from under grandmas sink\n" \
+            "tried and true. obscure. the dates are always older than me\n"\
+            "Bird house in the sink, cumming al in your pants when she comes over\n"\
+                 "skittles but all by himself\n" \
+             "A great many things are wrong with him\n" \
+             " And boils are never as sharp as the tacs\n"
+
+        return string
+
+    def Marinol2(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = " bowed graced with simple pleasures    god can replace laxatives with anything\n" \
+                 "          trim that water down to fit in the sink\n" \
+                 "her       ass         breaks      bones\n" \
+                 "And was never simple      to begin        with\n" \
+                 "Never trust theater geeks\n"
+
+        return string
+
+    def Marinol3(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = " A problem over there is another persons problem\n" \
+                 "so against representation \n" \
+                 "that crosses the blood brain barrier\n" \
+                 "Phenylpiracetam banned in \n" \
+                 "several countries, must be some good stuff\n" \
+                 "Like wires crossed through doorways and handheld momments\n" \
+                 "a flash in the tip\n" \
+                 "screwed the baddest bitch\n" \
+                 "but if you drop ne pray\n" \
+                 "I land on my head\n"
+
+    def SuperHighway(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "Excelerate. get home faster \n" \
+             " it's all wearkng off. two hundred gone in a few days\n" \
+             " tasting blood in you throat\n" \
+             " they won't notice at wr\ork, they even share\n" \
+             " t    t   they gave me numbers\n" \
+             " \so their buddies can get through a twelve \n" \
+             " hour shift. Can only keep functioing, keep speeding\n" \
+             "          umtoll she gets piseed you wont come over\n" \
+             "      or yoou have to ask for a drivers license\n" \
+
+        return string
+
+
+class Without_the_Zeppelin(object):
+
+    def Before_The_Met_Me(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = ""
+        return string
+
+    def Done_With_Nothing(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "The sales socket fills with broken " + nouns.get_noun(nouns.noun_list) +"\n" \
+                 " She'd "+ verbs.get_verb(verbs.verb_list) + " her own towel\n" \
+                 " never mind the one she bought but the one she stole\n" \
+                 " crafty like widdowmakers, each sole another life down the drain\n" \
+                 " She runs to "+ verbs.get_verb(verbs.verb_list) +" the last call during\n" \
+                 " window shopping for things she could return, could get back and take\n" \
+                 " all at the same "  + nouns.get_noun(nouns.noun_list) +"\n" \
+                 " \n" \
+                 " \n" \
+                 " \n" \
+                 " \n" \
+                 " \n" \
+                 " \n"
+        return string
+
+    def Holdup(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = " High tide and holy roller, bound to cross \n" \
+                 " the boarders of bulima, fingers ready to take\n" \
+                 " the methadone, that white bottle I see her brother\n" \
+                 " take and mix with anything else. The anti high high\n" \
+                 " \n" \
+                 " he threatened me the first thing\n" \
+                 " \"you better not hurt my sister\"\n" \
+                 " I promised and continued walking while he\n" \
+                 " slowly drifted from side to side, a capture of\n" \
+                 " the sun when a camera won't focus\n" \
+                 " like he's dying a little more\n" \
+                 " each out of focus day\n" \
+                 " \n"
+
+
+        return string
+
+    def Magic_Sentences(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = sentences.magic_sentences[random.randint(0, 6)] + "\n" \
+              + sentences.magic_sentences[random.randint(0, 6)] + "\n" \
+        + sentences.magic_sentences[random.randint(0, 6)] + "\n" \
+        + sentences.magic_sentences[random.randint(0, 6)] + "\n"
+        return string
+
+class Her_Plan(object):
+
+    def Never_Like_An_Animal(self):
+        print "\n\n" + sys._getframe().f_code.co_name + divider()
+        string = "I would       have heard\n" \
+                 " about it, through some  "\
+        "               ham radio station\n" \
+        " directly behind my coworkers head, while\n" \
+        " he rubs his feet on the corner of office chairs\n" \
+        " spreading something into the air that can         attatch\n" \
+        " to a support list, make a ticket, call the cops\n" \
+        " save me from his naked            feet\n"
+
+        return string
+
+def divider():
+    str =  "\n--------------------------"
+    return str
+
+if __name__ == "__main__":
+    """ Test Code"""
+    a = Wednesday_24_1130()
+    b = The_Hunk_and_I()
+    c = Without_the_Zeppelin()
+    d = Her_Plan()
+    print "\n*********************"
+    print type(a).__name__.upper()
+    print "\n*********************"
+   # a.Gave_it_a_shot.__name__()
+    print a.Gave_it_a_shot()
+    print a.Girl_Whisperer()
+    print a.Wildabeast()
+    print a.WildabeastII()
+    print a.Rose()
+    print a.Build_IT()
+    print a.Best_Bundle2()
+    # print a.
+    print "\n*********************"
+    print type(b).__name__.upper()
+    print "\n*********************"
+    print b.The_Romantics()
+    print b.VirtualGettinIt()
+    print b.Virtious_Game_Changer()
+    print b.Too_Much_Time_With_Chronos()
+    print b.Follow_with_care()
+
+    print "\n*********************"
+    print type(c).__name__.upper()
+    print c.Before_The_Met_Me()
+    print c.Done_With_Nothing()
+    print c.Magic_Sentences()
+    print "\n*********************"
+    print "\n*********************"
+    print type(d).__name__.upper()
+    print "\n*********************"
+
+    #print type(a).__name__
+
+def Demo(self):
+    print "\n\n" + sys._getframe().f_code.co_name + divider()
+    string = " \n" \
+    " \n"\
+    " \n"\
+    " \n" \
+    " \n" \
+    " \n" \
+    " \n" \
+    " \n" \
+    " \n" \
+    " \n" \
+    " \n" \
+    " \n" \
+    " \n"
+    return string
